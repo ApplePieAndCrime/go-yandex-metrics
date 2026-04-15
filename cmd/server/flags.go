ecc38363a1621d739ba3be29df8815578e24081d
Irina Anpodistova <irin.anpodistova@x5.ru>
2026-04-15 09:10:17 +0300

    env переменные для server 2

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -9,8 +9,8 @@ var flagRunAddress string
 
 func parseFlags() {
 	defaultAddress := "localhost:8080"
-	if serverPort := os.Getenv("SERVER_PORT"); serverPort != "" {
-		defaultAddress = "localhost:" + serverPort
+	if address := os.Getenv("ADDRESS"); address != "" {
+		defaultAddress = address
 	}
 
 	flag.StringVar(&flagRunAddress, "a", defaultAddress, "address and port to run server")
